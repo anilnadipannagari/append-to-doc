cfe613397f63cfd592035f481e8cb16f2d420e7f
Nadipannagari Anilkumar <111619322+anilnadipannagari@users.noreply.github.com>
2023-02-21 22:33:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let username=document.getElementById("inp");
+
+let newkid=document.getElementById('newkid')
+
+let submitbtn=document.getElementById('submitbtn');
+
+function validation(){
+   if(username.value !=""){
+        let li=document.createElement('li');
+        li.innerText=username.value;
+        newkid.append(li);
+    alert("your detailessaved sucessefullt=y")}else{
+        alert("please input check");
+    }
+
+    }
+// if(username.value !="" && pasword.value !=""){
+//     alert("Your detailes saved sucessfully");
+//              }
+// else{
+//     alert("please enter your data");    }
+// }
+
+// document.addEventListener(onclick,validation());
+submitbtn.onclick=validation ;
